e4f66d44220121a43dc7304da91a183de5bc9bc7
Aleksander Kuźma <aleksander.a.kuzma@gmail.com>
2023-11-04 12:23:38 +0000

    chore: initial commit

---
+++ lua/akselander/init.lua
@@ -0,0 +1,11 @@
+require("akselander.set")
+require("akselander.remap")
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+
