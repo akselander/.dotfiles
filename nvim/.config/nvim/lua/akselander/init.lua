f38e26f4d891ada9845210542f861dd4d9b314ca
Aleksander Kuźma <aleksander.a.kuzma@gmail.com>
2023-11-06 10:19:46 +0100

    chore: move nvim config to stow

---
+++ nvim/.config/nvim/lua/akselander/init.lua
@@ -0,0 +1,52 @@
+require("akselander.set")
+require("akselander.remap")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("akselander.lazy")
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
+local augroup = vim.api.nvim_create_augroup
+local AkselanderGroup = augroup('Akselander', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+autocmd({ "BufWritePre" }, {
+    group = AkselanderGroup,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
