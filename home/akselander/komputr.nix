482f7110d1f9806f772affecda5c10384cb9fb40
Aleksander Kuźma <akselander.a.kuzma@gmail.com>
2024-04-27 15:30:30 +0200

    correct monitors

--- home/akselander/komputr.nix
+++ home/akselander/komputr.nix
@@ -11,12 +11,12 @@
 
   wallpaper = lib.mkDefault pkgs.wallpapers.outer-wilds;
   colorscheme.source = config.wallpaper;
-  #  ------   -----   ------
-  # | DP-2 | | DP-3| | DP-1 |
-  #  ------   -----   ------
+  #  ------   -----   ---------
+  # | DP-2 | | DP-1| | HDMI-A-1|
+  #  ------   -----   ---------
   monitors = [
     {
-      name = "DP-2";
+      name = "DP-1";
       width = 1920;
       height = 1080;
       refreshRate = "239.76Hz";
@@ -28,14 +28,14 @@
       name = "DP-2";
       width = 2560;
       height = 1440;
-      x = 1920;
+      x = -1920;
       workspace = "2";
     }
     {
-      name = "DP-1";
+      name = "HDMI-A-1";
       width = 3840;
       height = 2160;
-      x = -1920;
+      x = 1920;
       workspace = "3";
       enabled = false;
     }
