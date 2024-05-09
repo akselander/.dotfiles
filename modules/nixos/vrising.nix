29744c1364114485aa802011084719c9cb6c7e74
Aleksander Kuźma <aleksander.a.kuzma@gmail.com>
2024-05-09 16:40:18 +0200

    vrising settings

--- modules/nixos/vrising.nix
+++ modules/nixos/vrising.nix
@@ -5,8 +5,227 @@
   ...
 }:
 with lib; let
+  settings = {
+    GameDifficulty = "2";
+    GameModeType = "PvP";
+    CastleDamageMode = "Never";
+    SiegeWeaponHealth = "Normal";
+    PlayerDamageMode = "Always";
+    CastleHeartDamageMode = "CanBeDestroyedOnlyWhenDecaying";
+    PvPProtectionMode = "Medium";
+    DeathContainerPermission = "ClanMembers";
+    RelicSpawnType = "Plentiful";
+    CanLootEnemyContainers = false;
+    BloodBoundEquipment = true;
+    TeleportBoundItems = true;
+    BatBoundItems = false;
+    AllowGlobalChat = true;
+    AllWaypointsUnlocked = false;
+    FreeCastleRaid = false;
+    FreeCastleClaim = false;
+    FreeCastleDestroy = false;
+    InactivityKillEnabled = true;
+    InactivityKillTimeMin = 3600;
+    InactivityKillTimeMax = 604800;
+    InactivityKillSafeTimeAddition = 172800;
+    InactivityKillTimerMaxItemLevel = 84;
+    StartingProgressionLevel = 0;
+    DisableDisconnectedDeadEnabled = true;
+    DisableDisconnectedDeadTimer = 60;
+    DisconnectedSunImmunityTime = 300.0;
+    InventoryStacksModifier = 1.0;
+    DropTableModifier_General = 1.25;
+    DropTableModifier_Missions = 1.0;
+    MaterialYieldModifier_Global = 1.0;
+    BloodEssenceYieldModifier = 1.0;
+    JournalVBloodSourceUnitMaxDistance = 25.0;
+    PvPVampireRespawnModifier = 1.0;
+    CastleMinimumDistanceInFloors = 2;
+    ClanSize = 10;
+    BloodDrainModifier = 1.0;
+    DurabilityDrainModifier = 0.5;
+    GarlicAreaStrengthModifier = 1.0;
+    HolyAreaStrengthModifier = 1.0;
+    SilverStrengthModifier = 1.0;
+    SunDamageModifier = 1.0;
+    CastleDecayRateModifier = 1.0;
+    CastleBloodEssenceDrainModifier = 1.0;
+    CastleSiegeTimer = 420.0;
+    CastleUnderAttackTimer = 60.0;
+    CastleRaidTimer = 600.0;
+    CastleRaidProtectionTime = 1800.0;
+    CastleExposedFreeClaimTimer = 300.0;
+    CastleRelocationCooldown = 10800.0;
+    CastleRelocationEnabled = true;
+    AnnounceSiegeWeaponSpawn = false;
+    ShowSiegeWeaponMapIcon = false;
+    BuildCostModifier = 1.0;
+    RecipeCostModifier = 1.0;
+    CraftRateModifier = 1.0;
+    ResearchCostModifier = 1.0;
+    RefinementCostModifier = 1.0;
+    RefinementRateModifier = 1.0;
+    ResearchTimeModifier = 1.0;
+    DismantleResourceModifier = 1.0;
+    ServantConvertRateModifier = 1.0;
+    RepairCostModifier = 1.0;
+    Death_DurabilityFactorLoss = 0.0;
+    Death_DurabilityLossFactorAsResources = 0.0;
+    StarterEquipmentId = 0;
+    StarterResourcesId = 0;
+    VBloodUnitSettings = [];
+    UnlockedAchievements = [];
+    UnlockedResearchs = [];
+    GameTimeModifiers = {
+      DayDurationInSeconds = 1080.0;
+      DayStartHour = 9;
+      DayStartMinute = 0;
+      DayEndHour = 17;
+      DayEndMinute = 0;
+      BloodMoonFrequency_Min = 10;
+      BloodMoonFrequency_Max = 18;
+      BloodMoonBuff = 0.2;
+    };
+    VampireStatModifiers = {
+      MaxHealthModifier = 1.0;
+      PhysicalPowerModifier = 1.0;
+      SpellPowerModifier = 1.0;
+      ResourcePowerModifier = 1.0;
+      SiegePowerModifier = 1.0;
+      DamageReceivedModifier = 1.0;
+      ReviveCancelDelay = 5.0;
+    };
+    UnitStatModifiers_Global = {
+      MaxHealthModifier = 1.0;
+      PowerModifier = 1.4;
+      LevelIncrease = 0;
+    };
+    UnitStatModifiers_VBlood = {
+      MaxHealthModifier = 1.25;
+      PowerModifier = 1.7;
+      LevelIncrease = 3;
+    };
+    EquipmentStatModifiers_Global = {
+      MaxHealthModifier = 1.0;
+      ResourceYieldModifier = 1.0;
+      PhysicalPowerModifier = 1.0;
+      SpellPowerModifier = 1.0;
+      SiegePowerModifier = 1.0;
+      MovementSpeedModifier = 1.0;
+    };
+    CastleStatModifiers_Global = {
+      TickPeriod = 5.0;
+      SafetyBoxLimit = 1;
+      EyeStructuresLimit = 1;
+      TombLimit = 12;
+      VerminNestLimit = 4;
+      PrisonCellLimit = 16;
+      HeartLimits = {
+        Level1 = {
+          FloorLimit = 50;
+          ServantLimit = 4;
+          BuildLimits = 2;
+          HeightLimit = 3;
+        };
+        Level2 = {
+          FloorLimit = 140;
+          ServantLimit = 5;
+          BuildLimits = 2;
+          HeightLimit = 3;
+        };
+        Level3 = {
+          FloorLimit = 240;
+          ServantLimit = 6;
+          BuildLimits = 2;
+          HeightLimit = 3;
+        };
+        Level4 = {
+          FloorLimit = 360;
+          ServantLimit = 7;
+          BuildLimits = 2;
+          HeightLimit = 3;
+        };
+        Level5 = {
+          FloorLimit = 550;
+          ServantLimit = 8;
+          BuildLimits = 2;
+          HeightLimit = 3;
+        };
+      };
+      CastleLimit = 2;
+      NetherGateLimit = 1;
+      ThroneOfDarknessLimit = 1;
+    };
+    PlayerInteractionSettings = {
+      TimeZone = "Local";
+      VSPlayerWeekdayTime = {
+        StartHour = 20;
+        StartMinute = 0;
+        EndHour = 22;
+        EndMinute = 0;
+      };
+      VSPlayerWeekendTime = {
+        StartHour = 20;
+        StartMinute = 0;
+        EndHour = 22;
+        EndMinute = 0;
+      };
+      VSCastleWeekdayTime = {
+        StartHour = 20;
+        StartMinute = 0;
+        EndHour = 22;
+        EndMinute = 0;
+      };
+      VSCastleWeekendTime = {
+        StartHour = 20;
+        StartMinute = 0;
+        EndHour = 22;
+        EndMinute = 0;
+      };
+    };
+    TraderModifiers = {
+      StockModifier = 1.0;
+      PriceModifier = 1.0;
+      RestockTimerModifier = 1.0;
+    };
+    WarEventGameSettings = {
+      Interval = 1;
+      MajorDuration = 1;
+      MinorDuration = 1;
+      WeekdayTime = {
+        StartHour = 0;
+        StartMinute = 0;
+        EndHour = 23;
+        EndMinute = 59;
+      };
+      WeekendTime = {
+        StartHour = 0;
+        StartMinute = 0;
+        EndHour = 23;
+        EndMinute = 59;
+      };
+      ScalingPlayers1 = {
+        PointsModifier = 1.0;
+        DropModifier = 1.0;
+      };
+      ScalingPlayers2 = {
+        PointsModifier = 0.5;
+        DropModifier = 0.5;
+      };
+      ScalingPlayers3 = {
+        PointsModifier = 0.25;
+        DropModifier = 0.25;
+      };
+      ScalingPlayers4 = {
+        PointsModifier = 0.25;
+        DropModifier = 0.25;
+      };
+    };
+  };
+  settingsFile = pkgs.writeText "settings.json" (builtins.toJSON settings);
+
   cfg = config.services.vrising-server;
-  wine = pkgs.wine.override { wineBuild = "wine64"; };
+  wine = pkgs.wine.override {wineBuild = "wine64";};
   shutdownScript =
     pkgs.writeShellScript "shutdown"
     ''
@@ -72,7 +291,6 @@ in {
   };
 
   config = mkIf cfg.enable {
-
     systemd.services.vrising-server = let
       steamcmd = "${pkgs.steamcmd}/bin/steamcmd";
     in {
@@ -90,9 +308,7 @@ in {
             -queryPort ${toString cfg.queryPort} \
             -lowerFPSWhenEmpty true \
             -password kremowka \
-            -saveName ${cfg.saveName} \
-            -preset StandardPvP_NoSiege \
-            -difficultyPreset Difficulty_Brutal
+            -saveName ${cfg.saveName}
         '';
         ExecStop = "${pkgs.bash}/bin/bash ${shutdownScript}";
         Restart = "on-failure";
@@ -103,6 +319,9 @@ in {
 
       preStart = ''
         ${steamcmd} +@sSteamCmdForcePlatformType windows +force_install_dir "/var/lib/vrising/server" +login anonymous +app_update 1829350 validate +quit
+	mkdir -p /var/lib/vrising/server/Settings
+	rm -f /var/lib/vrising/data/Settings/ServerGameSettings.json
+	ln -s ${settingsFile} /var/lib/vrising/data/Settings/ServerGameSettings.json
       '';
     };
 
