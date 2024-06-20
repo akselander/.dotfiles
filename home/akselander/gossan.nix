4803f5969d46bad20a4159271333569e76331c4c
Aleksander Kuźma <aleksander.a.kuzma@gmail.com>
2024-06-20 22:02:41 +0200

    feat(gossan): add monitors

--- home/akselander/gossan.nix
+++ home/akselander/gossan.nix
@@ -15,4 +15,26 @@
 
   wallpaper = lib.mkDefault pkgs.wallpapers.outer-wilds;
   colorScheme = inputs.nix-colors.colorSchemes.rose-pine;
+
+  #  ------   -----
+  # | DP-2 | | DP-1|
+  #  ------   -----
+  monitors = [
+    {
+      name = "eDP-1";
+      width = 1920;
+      height = 1200;
+      refreshRate = 60.00;
+      y = -1440;
+      workspace = "8";
+    }
+    {
+      name = "DP-2";
+      width = 2560;
+      height = 1440;
+      refreshRate = 59.95;
+      workspace = "1";
+      primary = true;
+    }
+  ];
 }
