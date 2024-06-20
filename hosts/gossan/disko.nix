f16b72255e5a2ef116382d6572ef8284b7bfce4a
Aleksander Kuźma <aleksander.a.kuzma@gmail.com>
2024-06-20 09:59:59 +0000

    chore(gossan): reformat with crypt

--- hosts/gossan/disko.nix
+++ hosts/gossan/disko.nix
@@ -6,66 +6,61 @@
       content = {
         type = "gpt";
         partitions = {
-          boot = {
-            name = "boot";
-            size = "1M";
-            type = "EF02";
-          };
-          esp = {
-            name = "ESP";
-            size = "500M";
-            type = "EF00";
-            content = {
+	  ESP = {
+	    label = "boot";
+	    name = "ESP";
+	    size = "1024M";
+	    type ="EF00";
+	    content = {
               type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-            };
-          };
-          swap = {
-            size = "4G";
-            content = {
-              type = "swap";
-              resumeDevice = true;
-            };
-          };
-          root = {
-            name = "root";
+	      format = "vfat";
+	      mountpoint = "/boot";
+	      mountOptions = [
+	        "defaults"
+	        "umask=0077"
+	      ];
+	    };
+	  };
+	  luks = {
             size = "100%";
-            content = {
-              type = "lvm_pv";
-              vg = "root_vg";
-            };
-          };
-        };
-      };
-    };
-    lvm_vg = {
-      root_vg = {
-        type = "lvm_vg";
-        lvs = {
-          root = {
-            size = "100%FREE";
-            content = {
-              type = "btrfs";
-              extraArgs = ["-f"];
+	    label = "gossan";
+	    content = {
+	      type = "luks";
+	      name = "cryptroot";
+	      extraOpenArgs = [
+	        "--allow-discards"
+	        "--perf-no_read_workqueue"
+	        "--perf-no_write_workqueue"
+	      ];
 
-              subvolumes = {
-                "/root" = {
-                  mountpoint = "/";
-                };
-
-                "/persist" = {
-                  mountOptions = ["subvol=persist" "noatime"];
-                  mountpoint = "/persist";
-                };
-
-                "/nix" = {
-                  mountOptions = ["subvol=nix" "noatime"];
-                  mountpoint = "/nix";
-                };
-              };
-            };
-          };
+	      settings = {
+	        # https://0pointer.net/blog/unlocking-luks2-volumes-with-tpm2-fido2-pkcs11-security-hardware-on-systemd-248.html
+	        crypttabExtraOpts = ["fido-2-device=auto" "token-timeout=10"];
+	      };
+	      content = {
+	        type = "btrfs";
+	        extraArgs = ["-L" "nixos" "-f"];
+	        subvolumes = {
+	          "/root" = {
+	            mountpoint = "/";
+	            mountOptions = ["subvol=root" "compress=zstd" "noatime"];
+	          };
+	          "/nix" = {
+	            mountpoint = "/nix";
+	            mountOptions = ["subvol=nix" "compress=zstd" "noatime"];
+	          };
+	          "/persist" = {
+	            mountpoint = "/persist";
+	            mountOptions = ["subvol=persist" "compress=zstd" "noatime"];
+	          };
+	          "/swap" = {
+	            mountpoint = "/swap";
+		    swap.swapfile.size = "64G";
+	          };
+	        };
+	      };
+	    };
+	  };
         };
       };
     };
