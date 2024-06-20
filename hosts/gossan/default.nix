25cb07e27f50773d9bf5b8067cd0d49e5d28ea18
asdf <asdf>
2024-06-20 15:04:37 +0200

    chore(gossan): remove encrypted-root

--- hosts/gossan/default.nix
+++ hosts/gossan/default.nix
@@ -14,7 +14,6 @@
     ../common/users/akselander
 
     ../common/optional/nuke-root.nix
-    ../common/optional/encrypted-root.nix
     ../common/optional/greetd.nix
     ../common/optional/pipewire.nix
     ../common/optional/quietboot.nix
