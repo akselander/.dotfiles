8da4280b14369aceaaf44d1c7863941a0cde71cb
Aleksander Kuźma <aleksander.a.kuzma@gmail.com>
2024-06-21 13:45:41 +0200

    fix(tmux): use pkgs.tmux in the scripts

--- home/akselander/features/cli/tmux.nix
+++ home/akselander/features/cli/tmux.nix
@@ -1,31 +1,37 @@
 {pkgs, ...}: let
+  tmux = "${pkgs.tmux}/bin/tmux";
+  fzf ="${pkgs.fzf}/bin/fzf";
   sessionizerScript =
     pkgs.writeShellScript "sessionizer"
     ''
-      #!/usr/bin/env bash
-      if [[ $# -eq 1 ]]; then
+      if [ $# -eq 1 ]; then
           selected=$1
       else
-          selected=$(find ~/work/zerodownhomes ~/projects ~/ ~/work ~/personal -mindepth 1 -maxdepth 1 -type d | fzf)
+          selected=$(find ~/work/zerodownhomes ~/projects ~/ ~/work ~/personal -mindepth 1 -maxdepth 1 -type d | ${fzf})
       fi
 
-      if [[ -z $selected ]]; then
+
+      if [ -z $selected ]; then
           exit 0
       fi
 
       selected_name=$(basename "$selected" | tr . _)
       tmux_running=$(pgrep tmux)
 
-      if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-          tmux new-session -s $selected_name -c $selected
+      if [ -z $TMUX ] && [ -z $tmux_running ]; then
+          ${tmux} new-session -s $selected_name -c $selected
           exit 0
       fi
 
-      if ! tmux has-session -t=$selected_name 2> /dev/null; then
-          tmux new-session -ds $selected_name -c $selected
+      if ! ${tmux} has-session -t=$selected_name 2> /dev/null; then
+          ${tmux} new-session -ds $selected_name -c $selected
+      fi
+      if [ -z $TMUX ]; then
+        ${tmux} attach-session -t $selected_name
+        exit 0
       fi
 
-      tmux switch-client -t $selected_name
+      ${tmux} switch-client -t $selected_name
     '';
 in {
   programs.tmux = {
@@ -51,9 +57,9 @@ in {
       bind -r h select-pane -L
       bind -r l select-pane -R
 
-      bind -r D neww -c "#{pane_current_path}" "[[ -e TODO.md ]] && nvim TODO.md || nvim ~/.dotfiles/personal/todo.md"
+      bind -r D neww -c "#{pane_current_path}" "[ -e TODO.md ] && nvim TODO.md || nvim ~/.dotfiles/personal/todo.md"
 
-      bind-key -r f run-shell "tmux neww ${sessionizerScript}"
+      bind-key -r f run-shell "${tmux} neww ${sessionizerScript}"
 
       bind-key -r G run-shell "${sessionizerScript} ~/work/zerodownhomes/curb"
       bind-key -r C run-shell "${sessionizerScript} ~/work/zerodownhomes/foyer"
@@ -80,7 +86,7 @@ in {
       lua
       */
       ''
-        vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww ${sessionizerScript}<CR>")
+        vim.keymap.set("n", "<C-f>", "<cmd>silent !${tmux} neww ${sessionizerScript}<CR>")
       '';
   };
 }
